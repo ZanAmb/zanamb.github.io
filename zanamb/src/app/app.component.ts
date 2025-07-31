32c178252269aa3a41344765d6c6bb76fc7453ed
ZanAmb <67069383+ZanAmb@users.noreply.github.com>
2025-07-31 17:50:28 +0200

    Fix reroute
    Attempt to fix 404 if loading into non-root path.

--- zanamb/src/app/app.component.ts
+++ zanamb/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -10,12 +11,19 @@ import { TranslateService } from '@ngx-translate/core';
 
 export class AppComponent {
   title = 'zanamb';
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private router: Router) {
   translate.addLangs(['en', 'sl']);
   translate.setDefaultLang('en');
 
   const browserLang = translate.getBrowserLang() || 'sl';
   const savedLang = localStorage.getItem('lang');
   translate.use(savedLang || browserLang?.match(/en|sl/) ? browserLang : 'en');
+
+  const redirect = sessionStorage.getItem('redirect');
+    if (redirect) {
+      sessionStorage.removeItem('redirect');
+      const relativeUrl = redirect.substring(location.origin.length);
+      this.router.navigateByUrl(relativeUrl);
+    }
 }
 }
